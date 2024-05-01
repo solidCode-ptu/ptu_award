d710118d1454994d656a8098e4f877ece10651e0
gbwlxhd97 <gbwlxhd97@naver.com>
2024-05-01 14:26:26 +0900

    init:

---
+++ src/main/java/com/example/ptu/ptu_award/PtuAwardApplication.java
@@ -0,0 +1,13 @@
+package com.example.ptu.ptu_award;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtuAwardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PtuAwardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ptu/ptu_award/PtuAwardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ptu.ptu_award;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PtuAwardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
