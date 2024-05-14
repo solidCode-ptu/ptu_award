36c74976a0e38e85f04f7dd16933ebe38c9e9433
JYL35 <dlwodbs1223@naver.com>
2024-05-14 23:43:54 +0900

    Create Filter Function

---
+++ src/main/java/com/example/ptu/ptu_award/models/FilterAward.java
@@ -0,0 +1,13 @@
+package com.example.ptu.ptu_award.models;
+
+public record FilterAward(
+        String title,
+        String department_name,
+        String description,
+        String filter_point,
+        String point,
+        String date_period,
+        String contact_info,
+        String link
+) {
+}
